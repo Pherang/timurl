7c20f669e46eb1bbcfde81237128cc7906e19444
Pherang <pherang@users.noreply.github.com>
2018-03-23 17:21:17 -0400

    Additional initialization of app

--- timurl.js
+++ timurl.js
@@ -2,4 +2,7 @@ const express = require('express'),
       MongoClient = require('mongodb').MongoClient,
       app = express();
 
+let uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/timmieurls'
 
+
+console.log(uri)
