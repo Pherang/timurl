5fc6bd16f04ef404654b656f7267043658729030
Pherang <pherang@users.noreply.github.com>
2018-03-24 12:47:19 -0400

    Added code to handle Short URLs not found

--- timurl.js
+++ timurl.js
@@ -8,6 +8,15 @@ let collection = 'urls'
 let hostname = process.env.APP_HOST || 'http://127.0.0.1:3000'
 MongoClient.connect(uri, function (err, client) {
   const db = client.db(dbName)
+
+  // Tries to retrieve short URL
+  async function checkShortUrl(urlNumber) {
+    let findings = await db.collection(collection).find({ timurlnumber: urlNumber})
+    let answer = await findings.next()
+    debugger
+    return answer
+  }
+
   app.get('/', (req,res) => {
    
     let msg = 'Hello, to use this service paste your url after http://timurl.herokuapp.com/tiny/' 
@@ -15,7 +24,7 @@ MongoClient.connect(uri, function (err, client) {
 
     res.send(msg)
   })
-
+  
   app.get(/tiny\/.*/, (req,res) => {
    
     // cleanup url for checking
@@ -23,7 +32,7 @@ MongoClient.connect(uri, function (err, client) {
     
     let validUrl
     // Just check if the URL format is correct upto the first / or full domain e.g. map.cork.app.com, gold.com/gimme
-    //thanks https://regexer.com gskinner for this tool. Did have to tweak it for JS to work      
+    //thanks https://regexer.com gskinner for this to test our Regexes in a sandbox
     let urlExpression = /http:\/\/(?:\w+)(?:\.\w+)+(?:\/|$)/i
 
     if(urlExpression.test(url)) {
@@ -44,49 +53,70 @@ MongoClient.connect(uri, function (err, client) {
         randomTimNumber = Math.floor(randomTimNumber)
         console.log('Random URL is ' + randomTimNumber)
 
+        // Find the document that has the short url number
         let result = await checkShortUrl(randomTimNumber)
         console.log('Does the ' +randomTimNumber+ ' exist? ' + result)
         
         if (result) {
           console.log('URL Found regenerating new URL')
-          debugger
+          
           // Recursive call to genShortUrl 
           return (randomTimNumber = await genShortUrl())
         } else {
           // If no matching short URL number is found then we can use the current one.
           console.log('The number doesn\'t exist ')
-          debugger
+          
           console.log('Returning valid URL ' + randomTimNumber)
           return randomTimNumber
         }
       }
       // Retrieve URL from database
-      async function checkShortUrl(urlNumber) {
-        let findings = await db.collection(collection).find({ timurlnumber: urlNumber})
-        let answer = await findings.next()
-        return answer
-      }
       async function insertUrl () {
         let urlNum = await genShortUrl(1234)
-        debugger
+        
         doc.timurlnumber = urlNum
-        debugger
+        
         db.collection(collection).insertOne(doc)
         let shortUrlObject = await db.collection(collection).find({ timurlnumber:urlNum })
         let answer = await shortUrlObject.next()
         console.log(answer)
         let resultsForUser = {}
         resultsForUser.fullurl = answer.fullurl
-        resultsForUser.shorturl = hostname + answer.timurlnumber
+        resultsForUser.shorturl = hostname + '/' + answer.timurlnumber
 
         res.send(resultsForUser)
         res.end()
       }
       insertUrl()
     } // End if statement validating URL
-    
+  
 
-  }) // end api endpoint route
+  }) // end get short url api endpoint route
+
+  // Check for four digit short url
+  app.get(/\/\d{4}$/, (req,res) => {
+    
+    let inputNumber = Number.parseInt(req.url.substring(1))
+    console.log(inputNumber)
+    async function getShortUrl(timnumber){
+      let resultObj = await checkShortUrl(timnumber)  
+      debugger
+      if (resultObj) {
+        let fullurl = resultObj.fullurl
+        console.log(fullurl)
+      } else {
+        res.send('No URL found')
+      }
+      res.end()
+    }
+    debugger
+    getShortUrl(inputNumber)
+  })
+
+  app.use( (req,res,next) => {
+    res.send('Error, go to home page for instructions')
+    res.end()
+  })
 
   let server = app.listen(3000, () => {
     let port = server.address().port
