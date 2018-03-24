f3167b5294f22a67d8feba958fe02f950e6be56f
Pherang <pherang@users.noreply.github.com>
2018-03-24 04:48:59 -0400

    Added code to generate 4 digits to replace URL and store in db

--- timurl.js
+++ timurl.js
@@ -3,9 +3,10 @@ const express = require('express'),
       app = express();
 
 let uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/timmieurls'
-
+let dbName = 'timmieurls'
+let collection = 'urls'
 MongoClient.connect(uri, function (err, client) {
-
+  const db = client.db(dbName)
   app.get('/', (req,res) => {
    
     let msg = 'Hello, to use this service paste your url after http://timurl.herokuapp.com/tiny/' 
@@ -21,20 +22,31 @@ MongoClient.connect(uri, function (err, client) {
     
     let validUrl
     // Just check if the URL format is correct upto the first / or full domain e.g. map.cork.app.com, gold.com/gimme
-    //let urlExpression = /http:\/\/(\w+)(\.\w+)+[/\s]/i;    //thanks https://regexer.com gskinner for this great tool!      
+    //thanks https://regexer.com gskinner for this tool. Did have to tweak it for JS to work      
     let urlExpression = /http:\/\/(?:\w+)(?:\.\w+)+(?:\/|$)/i
-    console.log(urlExpression)
-    console.log(url)
-    console.log(urlExpression.test(url))
+
     if(urlExpression.test(url)) {
       validUrl = url
       console.log('It is a valid url ' + validUrl)
+      let doc = {}
+      let randomTimNumber
+      doc.fullurl = validUrl
+
+      // Create a random number and check to see if it is in the DB. If it is, it has to be regenerated.
+      randomTimNumber = Math.random()
+      console.log(randomTimNumber)
+      randomTimNumber = randomTimNumber * 10000
+      if (randomTimNumber < 1000) { randomTimNumber = randomTimNumber * 10 }
+      console.log(Math.floor(randomTimNumber))
+      doc.timurlnumber = Math.floor(randomTimNumber)
+    
+//      db.collection(collection).insertOne(doc) 
       res.send(validUrl)
     }
     
     res.end()
 
-  })
+  }) // end api endpoint route
 
   let server = app.listen(3000, () => {
     let port = server.address().port
