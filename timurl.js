08ba12978d0cbdd5d5847cfc42a50d00efb0d8bc
Pherang <pherang@users.noreply.github.com>
2018-03-23 20:49:57 -0400

    Added route to app to handle /tiny/ endpoint

--- timurl.js
+++ timurl.js
@@ -4,5 +4,26 @@ const express = require('express'),
 
 let uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/timmieurls'
 
+MongoClient.connect(uri, function (err, client) {
+app.get('/', (req,res) => {
+   
+    let msg = 'Hello, to use this service paste your url after http://timurl.herokuapp.com/tiny/' 
+    msg += '<br>E.g http://timurl.herokuapp.com/tiny/https://developer.mozilla.org'
 
-console.log(uri)
+    res.send(msg)
+  })
+
+
+  app.get(/tiny\/.*/, (req,res) => {
+   
+    res.send(req.url)
+    res.end()
+  })
+
+  let server = app.listen(3000, () => {
+    let port = server.address().port
+    console.log('Express server listening on port %s.', port)
+  })
+
+
+});
